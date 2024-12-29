cbd6b79ccf98ed024c5da07b885a3a3ed60a961b
Mikhail <mikhailvillanueva0216@gmail.com>
2024-12-30 01:55:07 +0800

    mikhail

--- quizapp/app_main/urls.py
+++ quizapp/app_main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("register/", views.register_view, name="register_view"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("logout/", LogoutView.as_view(next_page='login_view'), name="logout"),
+    path("student_dashboard/", views.student_dashboard, name="student_dashboard"),
 ]
--- quizapp/student_app/urls.py
+++ quizapp/student_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from . import views 
 
 urlpatterns = [
-    path("", views.dashboard, name="students_dashboard"),
+    path("students_dashboard/", views.dashboard, name="students_dashboard"),
     path('join_class/', views.join_class, name='join_class'),
     path("user_dashboard/", views.user_dashboard, name="user_dashboard"),
 ]
